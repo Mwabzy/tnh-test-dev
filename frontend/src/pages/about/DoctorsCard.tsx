0df107fbb570d939fd7e52c30355391129ffc231
Mwabzy <eb5013667@gmail.com>
2025-10-09 14:57:26 +0300

    message

--- frontend/src/pages/about/DoctorsCard.tsx
+++ frontend/src/pages/about/DoctorsCard.tsx
@@ -39,7 +39,7 @@ const DoctorBrief: React.FC<TeamPageProps> = ({
 
       <section className="py-12 px-4 bg-gray-100">
         <div className="max-w-7xl mx-auto">
-          <div className="grid  place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
             {members.map((member, index) => (
               <TeamCard key={index} {...member} />
             ))}
