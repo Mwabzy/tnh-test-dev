5e856e6edf47ef1caa57583edc8c1ecba177436e
ian <iansmithxv@gmail.com>
2025-12-01 07:37:04 +0300

    Quicklinks mobile responsive

--- frontend/src/components/quicklinks.tsx
+++ frontend/src/components/quicklinks.tsx
@@ -1,41 +1,36 @@
-import { Phone, Clock, MapPin } from "lucide-react";
+import { Phone, Ambulance, ClipboardClock } from "lucide-react";
 import { Link } from "react-router";
 
 export default function Quicklinks() {
   return (
-    <div className="w-full sticky top-0 z-[200] bg-[#8B1C1C] text-white text-sm h-11 flex items-center">
-      <div className="flex items-center justify-start px-4 w-full">
-        {/* Left Section */}
-        <div className="flex items-center space-x-6">
-          {/* Phone */}
-          <Link to="href:tel:+254202845000">
-            <div className="flex items-center space-x-2">
-              <Phone className="w-4 h-4 text-yellow-400" />
-              <span>
-                <span className="font-semibold">Emergency:</span> +254 20
-                2845000
-              </span>
+    <div className="flex  items-center px-4 py-1w-full sticky top-0 z-200 bg-[#8B1C1C] text-white text-sm h-11 ">
+      <div className="flex space-x-13 md:space-x-6">
+        <Link to="href:tel:+254202845000">
+          <div className="flex items-center space-x-2">
+            <Ambulance className="w-4 h-4 text-yellow-400" />
+            <div className="flex flex-col md:flex-row">
+              <span className="font-semibold">Emergency :</span>
+              <span>+254 20 2845000</span>
             </div>
-          </Link>
+          </div>
+        </Link>
 
-          {/* 24/7 Emergency Services */}
-          <Link to={`/booking-calendar?serviceId=1`}>
-            <div className="flex items-center space-x-2">
-              <Clock className="w-4 h-4 text-yellow-400" />
-              <span>Book an appointment</span>
+        <Link to={`/booking-calendar?serviceId=1`}>
+          <div className="flex items-center space-x-2">
+            <ClipboardClock className="w-4 h-4 text-yellow-400" />
+            <div className="flex flex-col md:flex-row">
+              <span className="font-semibold">Book an</span>
+              <span>appointment</span>
             </div>
-          </Link>
+          </div>
+        </Link>
 
-          {/* Location */}
-          <Link to={"/contact-us"}>
-            <div className="flex items-center space-x-2">
-              <MapPin className="w-4 h-4 text-yellow-400" />
-              <span>Contact Us</span>
-            </div>
-          </Link>
-        </div>
-
-        {/* Right Section */}
+        <Link to={"/contact-us"}>
+          <div className="hidden md:flex items-center space-x-2">
+            <Phone className="w-4 h-4 text-yellow-400" />
+            <span>Contact Us</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
