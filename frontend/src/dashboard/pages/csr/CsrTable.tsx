ad393238d6539a220c1ed1764ff2f16ac7c5f3c3
Mwabzy <eb5013667@gmail.com>
2026-01-13 16:31:03 +0300

    csr form

---
+++ frontend/src/dashboard/pages/csr/CsrTable.tsx
@@ -0,0 +1,5 @@
+const CsrTable = () => {
+  return <div>csr</div>;
+};
+
+export default CsrTable;
---
+++ frontend/src/types/csr.ts
@@ -0,0 +1,12 @@
+export interface CSR {
+  id: number;
+  author: string;
+  title: string;
+  subtitle: string;
+  blogsubtitle: string;
+  description: string;
+  shortdesc: string;
+  longdesc: string;
+  coverImage: string;
+  image: string[];
+}
