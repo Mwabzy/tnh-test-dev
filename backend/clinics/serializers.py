71e3c2864169bdfca0bd5ceb17b00779acbe4048
Goro Collins <gorocollins@gmail.com>
2026-01-19 15:55:24 +0300

    use the url attribute for image

--- backend/clinics/serializers.py
+++ backend/clinics/serializers.py
@@ -177,16 +177,17 @@ class TestimonialSerializer(serializers.ModelSerializer):
 
 class ClinicalServiceImageSerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField()
+    image = serializers.ImageField(max_length=100, allow_null=True, required=False)
 
     class Meta:
         model = ClinicalServiceImage
-        fields = ["id", "url", "alt"]
+        fields = ["id", "url", "image", "alt"]
 
     def get_url(self, obj):
         request = self.context.get("request")
         if request:
-            return request.build_absolute_uri(obj.image.url)
-        return obj.image.url
+            return obj.image.url
+        # return obj.image.url
 
 
 # ClinicalService Serializer
