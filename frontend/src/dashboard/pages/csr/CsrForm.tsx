60df6a58afc5f4e38c85bb4a209ba9807d0d4dd6
Goro Collins <gorocollins@gmail.com>
2026-01-20 08:47:37 +0300

    expected void from API comms

--- frontend/src/dashboard/pages/csr/CsrForm.tsx
+++ frontend/src/dashboard/pages/csr/CsrForm.tsx
@@ -4,7 +4,7 @@ import { CSR } from "@/types";
 
 interface Props {
   initialData?: CSR | null;
-  onSave: (data: FormData) => Promise<any>;
+  onSave: (data: FormData) => Promise<void>;
   onCancel: () => void;
 }
 
