d1a38742172065a1ffcd61dd44bc9717113a0bbf
ian <iansmithxv@gmail.com>
2026-04-20 10:24:26 +0300

    Added TokenObtainPair to log in flow

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from authentication.views import RegisterView, LoginView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('tnh-adm/', admin.site.urls),
 
-    path('api/v1/auth/register/', RegisterView.as_view(), name='api-register'),
-    path('api/v1/auth/login/', LoginView.as_view(), name='api-login'),
-
+    path('api/v1/auth/', include('authentication.urls')),
 
     path('api/v1/', include('clinics.urls')),
     path('api/v1/', include('generalinfo.urls')),
